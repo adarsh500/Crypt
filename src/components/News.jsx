ab42452fd55c3026c22cbd6935446154b3e3f414
adarsh500 <adarshsulegai50@gmail.com>
2022-03-31 00:27:10 +0530

    feat: improve news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
-import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import { Select, Typography, Row, Col, Avatar, Card, Input } from "antd";
 
+import { useGetCryptosQuery } from "../services/cryptoApi";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { Option } = Select;
 
 const News = ({ simplified }) => {
-
-  const placeholderImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg'
-  const count = simplified ? 6 : 10;
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const placeholderImage =
+    "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+  const count = simplified ? 6 : 12;
+  const { data: cryptosList } = useGetCryptosQuery(count);
   const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
-    newsCategory: "Cryptocurrency",
+    newsCategory,
     count,
   });
 
@@ -21,6 +24,25 @@ const News = ({ simplified }) => {
   console.log(cryptoNews);
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a coin"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(nput, option) =>
+              option.children.toLowerCase().indexOf(Input.toLowerCase()) >= 0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {cryptosList?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, index) => (
         <Col xs={24} md={12} lg={8} key={index}>
           <Card hoverable className="news-card">
@@ -36,16 +58,27 @@ const News = ({ simplified }) => {
                 />
               </div>
               <p>
-                {news.description > 100 ? `${news.description.substring(0, 100)}...` : news.description}
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
               </p>
               <div className="provider-container">
                 <div>
-
-                <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || placeholderImage} alt="news" />
-                  <Text className="provider-name">{ news.provider[0]?.name}</Text>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      placeholderImage
+                    }
+                    alt="news"
+                  />
+                  <Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Text>
                 </div>
-                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
-            </div>
+                <Text>
+                  {moment(news.datePublished).startOf("ss").fromNow()}
+                </Text>
+              </div>
             </a>
           </Card>
         </Col>
